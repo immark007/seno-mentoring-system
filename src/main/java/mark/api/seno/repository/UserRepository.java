343f762627e166a492620f9618230f2f16cd9deb
immark007 <immark5507@gmail.com>
2025-03-06 14:48:37 -0300

    feat: add UserRepository for user data access

---
+++ src/main/java/mark/api/seno/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package mark.api.seno.repository;
+
+import mark.api.seno.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
