0aadba4623f75e512c61d82d151ae039c5ba9361
immark007 <immark5507@gmail.com>
2025-03-06 14:47:33 -0300

    feat: add User entity with UUID primary key

---
+++ src/main/java/mark/api/seno/model/user/User.java
@@ -0,0 +1,48 @@
+package mark.api.seno.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mark.api.seno.model.student.Student;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column
+    private UUID id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Role roles;
+
+    @CreatedDate
+    @Column
+    private LocalDateTime registration_date;
+
+    @LastModifiedDate
+    private LocalDateTime update_date;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Student student;
+}
